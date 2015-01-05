f146453c6e09225d1f93df0ecac33f9cbfc44c46
SylviaLi-Y <neerGGreen@live.com>
2015-01-05 11:47:09 +0800

    不含需求3的需求4的Pos.java

--- src/domains/Pos.java
+++ src/domains/Pos.java
@@ -1,29 +1,36 @@
-﻿package domains;
+package domains;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Sylvia on 2014/12/30.
  */
 public class Pos {
+    ArrayList<Item> giftProduct=new ArrayList<Item>();
+
     public String getShoppingList(ShoppingChart shoppingChart) {
         ArrayList<Item> items = shoppingChart.getItems();
         double total=0.0;
-        double save=0.00;
+        double save=0.0;
 
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("***商店购物清单***\n");
+        stringBuilder.append("***商店购物清单***\n")
+                .append("打印时间：")
+                .append(this.timeOfPrint())
+                .append("\n")
+                .append("----------------------\n");
 
-        for(int i=0;i<items.size();i++)
-        {
+        for(int i=0;i<items.size();i++) {
             Item item = items.get(i);
             int amountOfItem = item.getQuantity();
             double priceOfItem = item.getPrice();
             double discountOfItem=item.getDiscount();
             String nameOfItem = item.getName();
             String unitOfItem = item.getUnit();
-            double subTotal = priceOfItem * amountOfItem*discountOfItem;
-            save+=priceOfItem * amountOfItem*(1-discountOfItem);
+            double subTotal = this.calSubTotal(item);
+            save+=priceOfItem * amountOfItem-subTotal;
             total += subTotal;
             stringBuilder
                     .append("名称：").append(nameOfItem).append("，")
@@ -31,6 +38,17 @@ public class Pos {
                     .append("单价：").append(String.format("%.2f", priceOfItem)).append("(元)").append("，")
                     .append("小计：").append(String.format("%.2f", subTotal)).append("(元)").append("\n");
         }
+        if(giftProduct.size()>0){
+            stringBuilder
+                    .append("----------------------\n")
+                    .append("挥泪赠送商品：\n");
+            for(int i=0;i<giftProduct.size();i++){
+                stringBuilder
+                        .append("名称：").append(giftProduct.get(i).getName()).append("，")
+                        .append("数量：").append("1").append(giftProduct.get(i).getUnit()).append("\n");
+            }
+        }
+
         stringBuilder
                 .append("----------------------\n")
                 .append("总计：").append(String.format("%.2f", total)).append("(元)").append("\n");
@@ -41,5 +59,33 @@ public class Pos {
         stringBuilder.append("**********************\n");
 
         return stringBuilder.toString();
+    }
+
+    private double calSubTotal(Item item){
+        if(item.getDiscount()!=1){
+            //检查是否在活动期限之内
+            if(false){
+                return item.getQuantity()*item.getPrice();
+            }
+            return item.getQuantity()*item.getPrice()*item.getDiscount();
+        }
+        else if((item.isPromotion()==true)&&item.getQuantity()>3){
+            //检查是否在活动期限之内
+            if(false){
+                return item.getQuantity()*item.getPrice();
+            }
+            giftProduct.add(item);
+            return (item.getQuantity()-1)*item.getPrice();
+        }
+        else {
+            return item.getQuantity()*item.getPrice();
+        }
+    }
+
+    //获取当前时间
+    private String timeOfPrint(){
+        Date d=new Date();
+        DateFormat d5 = DateFormat.getDateTimeInstance(DateFormat.FULL,DateFormat.FULL); //显示日期，周，时间（精确到秒）
+        return d5.format(d);
     }
 }
